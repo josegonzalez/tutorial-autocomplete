59b5ad5df9722a6520be46a9eb02dd969128635b
Jose Diaz-Gonzalez <myaccounts@savant.be>
2011-06-25 11:55:27 -0400

    schema.sql

---
+++ config/schema/schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `posts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) DEFAULT NULL,
+  `content` text,
+  `tags` varchar(255) DEFAULT NULL,
+  `created` date DEFAULT NULL,
+  `active` tinyint(1) DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `link` (`link`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
